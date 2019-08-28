8b27169547882fbfd0ef4a85ad92dcdb02f93072
Hai Hua (Ryan) Tan <ryanhhtan@gmail.com>
2019-08-28 10:48:08 -0700

    Update install-dev-server-ubuntu-1604.sh
    add -y option in apt install

--- install-dev-server-ubuntu-1604.sh
+++ install-dev-server-ubuntu-1604.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 ## update apt-get
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 
 ## install docker and docker-compose
-sudo apt install docker.io
-sudo apt install docker-compose
+sudo apt install docker.io -y
+sudo apt install docker-compose -y
 sudo usermod -aG docker vagrant
 
 ## install nginx
-sudo apt install nginx
+sudo apt install nginx -y
 
 ## install nodejs 12
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install nodejs
+sudo apt install nodejs -y
 
 ## install basic utilities
-sudo apt install zip unzip httpie
+sudo apt install zip unzip httpie -y
 
 ## install ripgrep
 sudo add-apt-repository ppa:x4121/ripgrep
-sudo apt-get update
-sudo apt-get install ripgrep
+sudo apt-get update -y
+sudo apt-get install ripgrep -y
 
 ## install latest vim
 sudo add-apt-repository ppa:jonathonf/vim
-sudo apt install vim
+sudo apt install vim -y
 
 ## install sdkman
 curl -s "https://get.sdkman.io" | bash
