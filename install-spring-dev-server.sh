aa7f555a8f20b5774dfb55e44342bda103655f6e
Hai Hua (Ryan) Tan <ryanhhtan@gmail.com>
2019-08-28 12:38:17 -0700

    Update install-spring-dev-server.sh

--- install-spring-dev-server.sh
+++ install-spring-dev-server.sh
@@ -39,4 +39,4 @@ sdk install gradle
 sudo apt-get clean
 sudo dd if=/dev/zero of=/EMPTY bs=1M
 sudo rm -f /EMPTY
-cat /dev/null > ~/.bash_history && history -c && exit
+cat /dev/null > ~/.bash_history && history -c
