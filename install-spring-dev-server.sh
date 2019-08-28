28ccc10d608237cf3dbca2409ad56dd9147d2b91
Hai Hua (Ryan) Tan <ryanhhtan@gmail.com>
2019-08-28 12:37:20 -0700

    Update and rename install-dev-server-ubuntu-1604.sh to install-spring-dev-server.sh
    added clean up script

--- install-spring-dev-server.sh
+++ install-spring-dev-server.sh
@@ -35,3 +35,8 @@ sdk install maven
 sdk install springboot
 sdk install gradle
 
+## clean up
+sudo apt-get clean
+sudo dd if=/dev/zero of=/EMPTY bs=1M
+sudo rm -f /EMPTY
+cat /dev/null > ~/.bash_history && history -c && exit
