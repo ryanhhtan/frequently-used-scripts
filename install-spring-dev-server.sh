5339d8b48c1b150abedfe9ac7ed3928046ffec8d
Hai Hua (Ryan) Tan <ryanhhtan@gmail.com>
2019-09-25 12:21:14 -0700

    Update install-spring-dev-server.sh

--- install-spring-dev-server.sh
+++ install-spring-dev-server.sh
@@ -1,54 +1,59 @@
 #!/bin/bash
+[[ $(id -u) -ne 0 ]] && {
+  echo "please run as root or by sudo."
+  exit
+}
 
-## install make and gcc
-sudo apt install make -y
-sudo apt install gcc -y
+## install basic tools: make, gcc, zip and unzip
+apt install make gcc zip unzip -y
 
 ## install docker and docker-compose
-sudo apt install docker.io -y
-sudo apt install docker-compose -y
-sudo usermod -aG docker vagrant
+apt install docker.io -y
+apt install docker-compose -y
+usermod -aG docker vagrant
 
 ## install nginx
-sudo apt install nginx -y
+apt install nginx -y
 
-## install nvm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-source ~/.bashrc
-mvn install 10.16.3
 
 ## install basic utilities
-sudo apt install zip unzip httpie tmux -y
+apt install zip unzip httpie tmux -y
 
 ## install ripgrep
-sudo add-apt-repository ppa:x4121/ripgrep << EOF
+add-apt-repository ppa:x4121/ripgrep << EOF
 yes
 EOF
-sudo apt-get update -y
-sudo apt-get install ripgrep -y
+apt-get update -y
+apt-get install ripgrep -y
 
 ## install latest vim
-sudo add-apt-repository ppa:jonathonf/vim << EOF
+add-apt-repository ppa:jonathonf/vim << EOF
 
 EOF 
-sudo apt-get update -y
-sudo apt install vim -y
+apt-get update -y
+apt install vim -y
+
+## final update and upgrade
+apt-get update && sudo apt-get upgrade -y
 
 ## install sdkman
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 ## install java dev environment
+su vagrant
 sdk install java 8.0.222.j9-adpt
 sdk install maven
 sdk install springboot
 sdk install gradle
 
-## final update and upgrade
-sudo apt-get update && sudo apt-get upgrade -y
+## install nvm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+source ~/.bashrc
+mvn install 10.16.3
 
 ## clean up
-# sudo apt-get clean
-# sudo dd if=/dev/zero of=/EMPTY bs=1M
-# sudo rm -f /EMPTY
-# cat /dev/null > ~/.bash_history && history -c
+apt-get clean
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+cat /dev/null > ~/.bash_history && history -c
