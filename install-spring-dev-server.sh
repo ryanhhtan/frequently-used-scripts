ca8ea84d3db0c52c07fbf5476e3021dcf0214187
Hai Hua (Ryan) Tan <ryanhhtan@gmail.com>
2019-09-26 09:49:41 -0700

    Update install-spring-dev-server.sh

--- install-spring-dev-server.sh
+++ install-spring-dev-server.sh
@@ -56,7 +56,7 @@ sdk install springboot
 ## install nvm to user vagrant
 su vagrant -c 'wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash'
 source "${HOME}/.bashrc"
-nvm install 10.16.3
+su vagrant -c 'nvm install 10.16.3'
 
 ## clean up
 apt-get clean
