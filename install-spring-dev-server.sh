744f6270e4ba41c4e671a40f334c8c74ab308b45
ryanhhtan <ryanhhtan@gmail.com>
2019-09-28 10:16:52 -0700

    try to fix node not found
    Signed-off-by: ryanhhtan <ryanhhtan@gmail.com>

--- install-spring-dev-server.sh
+++ install-spring-dev-server.sh
@@ -53,7 +53,9 @@ sdk install gradle
 sdk install springboot
 
 ## install nvm to user vagrant
-su - vagrant -c 'wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash && source "/${HOME}/.nvm/nvm.sh" && nvm install 10.16.3'
+su - vagrant -c 'wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash'
+source "/${HOME}/.nvm/nvm.sh" 
+nvm install 10.16.3
 
 ## clean up
 apt-get clean
